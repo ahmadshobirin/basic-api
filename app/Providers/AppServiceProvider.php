e5f0a94461ba42c845f6c6a941cd6336067b398b
ahmadshobirin <ahmadshobirin.dev@gmail.com>
2020-11-14 12:41:16 +0700

    setup locale and timezone

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
+use Carbon\Carbon;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +25,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Schema::defaultStringLength(191); //handling mysql version <= MySQL v5.7.7
+        config(['app.locale' => 'id']);
+        Carbon::setLocale('id');
     }
 }
