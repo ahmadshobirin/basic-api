98142e2ecd4dd236a717d4ba709018a04fba1ee6
ahmadshobirin <ahmadshobirin.dev@gmail.com>
2020-11-14 12:34:56 +0700

    resource api users

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = User::select("id", "name", "email", "created_at")->orderBy("created_at", "desc")->get();
+
+        return response()->json([
+            "status" => 200,
+            "message" => "OK",
+            "data" => $users,
+        ]);
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            "email"    => "email|required|min:8|max:100|unique:users,email",
+            "name"     => "string|required|min:3",
+            "password" => "string|required|min:3"
+        ]);
+
+        $user = User::create([
+            "email" => $request->email,
+            "name" => $request->name,
+            "password" => bcrypt($request->password),
+        ]);
+
+        return response()->json([
+            "status" => 201,
+            "message" => "OK"
+        ], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $user = User::select("id", "name", "email", "created_at")->where('id',$id)->first();
+
+        if(!$user){
+            return response()->json([
+                "status" => 404,
+                "message" => "Requested data not found",
+            ], 404);
+        }
+
+        return response()->json([
+            "status" => 201,
+            "message" => "OK",
+            "data" => $user,
+        ], 201);
+    }
+
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            "name"     => "string|required|min:3",
+        ]);
+
+        User::where('id', $id)->update([
+            "name" => $request->name,
+        ]);
+
+        return response()->json([
+            "status" => 200,
+            "message" => "OK"
+        ], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $user = User::where('id', $id)->first();
+
+        if(!$user){
+            return response()->json([
+                "status" => 404,
+                "message" => "Requested data not found",
+            ], 404);
+        }
+
+        $user->delete();
+
+        return response()->json([
+            "status" => 200,
+            "message" => "OK",
+        ], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
@@ -30,4 +19,9 @@ Route::get("hello", function(){
     ]);
 });
 
-
+Route::get("users", [\App\Http\Controllers\Api\UserController::class, 'index']);
+Route::get("user/{id}", [\App\Http\Controllers\Api\UserController::class, 'show']);
+Route::post("user", [\App\Http\Controllers\Api\UserController::class, 'store']);
+Route::put("user/{id}", [\App\Http\Controllers\Api\UserController::class, 'update']);
+Route::patch("user/{id}", [\App\Http\Controllers\Api\UserController::class, 'update']);
+Route::delete("user/{id}", [\App\Http\Controllers\Api\UserController::class, 'destroy']);
